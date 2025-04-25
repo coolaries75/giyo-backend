aeb5392cbfc007811bfd16446587cb5f8fa788db
Zaher Audi <zaher.audi@gmail.com>
2025-04-25 07:30:20 -0400

    Added auto table creation to database.py

--- database.py
+++ database.py
@@ -3,17 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Fetch DATABASE_URL from environment variables
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set.")
 
-# Create engine
 engine = create_engine(DATABASE_URL)
-
-# SessionLocal class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base class for models
 Base = declarative_base()
+
+# Auto-create tables
+def init_db():
+    from models import db_service, db_brochure
+    Base.metadata.create_all(bind=engine)
+
+init_db()
